5559602314e6aaf2c12a68c2204090d315d8e244
yugandhar <142428339+yugandhar9786@users.noreply.github.com>
2025-03-05 22:54:54 +0530

    update

--- main.py
+++ main.py
@@ -2,26 +2,22 @@
 
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI() #to create an instance of fastapi
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
 
 @app.get("/") # to create a route "/" is the root route
 def welcome():
     return {"message": "welcome to fastapi tutorial"}
 
-@app.get("/about")
-def about():
-    return {"message": "this is an about page"}
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post.published)
+    return {"data": new_post}
 
-@app.get("/contact")
-def contact():
-    return {"message": "this page is about contact releated things"}
-
-@app.get("/blog")
-def blog():
-    return {"message": "this page is about blogs"}
-
-@app.post("/create posts")
-def create_posts(payload: dict= Body(...)):
-    print(payload)
-    return {"message": "successfully created a post"}
+# title 
